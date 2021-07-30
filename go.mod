59e24f3e3c0e3483e1e498cfe55305ceee899fdf
roma8ok <roma8ok@gmail.com>
2021-07-30 20:56:59 +0700

    project started

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/roma8ok/public-ip
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type response struct {
+	RemoteAddr    string `json:"remote_addr"`
+	XForwardedFor string `json:"x_forwarded_for"`
+	XRealIP       string `json:"x_real_ip"`
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	res := response{
+		RemoteAddr:    req.RemoteAddr,
+		XForwardedFor: req.Header.Get("X-Forwarded-For"),
+		XRealIP:       req.Header.Get("X-Real-Ip"),
+	}
+
+	resJSON, _ := json.Marshal(res)
+
+	fmt.Fprint(w, resJSON)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":80", nil)
+}
